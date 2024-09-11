9420a31be3e5323345fba1adcf048e2e2f733730
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-11 09:38:37 +0700

    Membuat halaman home page

---
+++ porfolio/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import Navbar from "../components/layouts/Navbar";
+// import Footer from "../components/layouts/Footer";
+
+const HomePage = () => {
+  return (
+    <div className="h-[2000px]">
+      Home Page
+      <div></div>
+    </div>
+  );
+};
+
+export default HomePage;
