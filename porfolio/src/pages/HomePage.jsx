4a9f9887b792f737ddfccb23174b3b2b5856a780
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-09-11 20:44:28 +0700

    Memanggil pages yang menjadi di pisah

--- porfolio/src/pages/HomePage.jsx
+++ porfolio/src/pages/HomePage.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-// import Navbar from "../components/layouts/Navbar";
-// import Footer from "../components/layouts/Footer";
+import Hero from "../components/layouts/Hero";
+import About from "../components/layouts/About";
+import Services from "../components/layouts/Services";
+import Project from "../components/layouts/Project";
 
 const HomePage = () => {
   return (
-    <div className="h-[2000px]">
-      Home Page
-      <div></div>
+    <div className="homepage pb-10">
+      <div className="container mx-auto px-4">
+        {/* hero */}
+        <Hero />
+
+        {/* about */}
+        <About />
+
+        {/* services */}
+        <Services />
+
+        {/* proyek */}
+        <Project />
+      </div>
     </div>
   );
 };
